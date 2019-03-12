a591f94d691874174b4e78f7e78f769367e4f43c
root <root@gpu006.ali.momo.com>
2019-03-12 14:44:43 +0800

    sep video and detect salient

--- testDirTxt.py
+++ testDirTxt.py
@@ -4,17 +4,28 @@ import cv2
 import numpy as np
 from Salicon import Salicon
 root = "/gpu/zhengmeisong/salient/data/"
-f = open(sys.argv[1])
-outDir = root + 's255_out/'
-if not os.path.isdir(outDir):
-    os.makedirs(outDir)
+f = open(root + sys.argv[1])
+sal = Salicon()
 for line in f:
     filepath = line.strip()
-    filename = filepath.split('/')[-1]
-    print filepath,filename
-    sal = Salicon()
-    map = sal.compute_saliency(root+filepath)
-    cv2.imwrite(outDir+filename, map*255)
-    np.savetxt(outDir+filename.split('.')[0]+'.txt', map*255, fmt="%d")
+    videoname = filepath.split('/')[-1].split('.')[0]
+    print videoname
+    outDir = root + '/' + videoname +'/'
+    if not os.path.isdir(outDir):
+        os.makedirs(outDir)
+    video = cv2.VideoCapture(root + filepath)
+    success, im = video.read()
+    numFrame = 0
+    while success:
+        oriname = videoname + '_o' + str(numFrame) + '.jpg'
+        salname = videoname + '_s' + str(numFrame) + '.jpg'
+        txtname = videoname + '_' + str(numFrame) + '.txt'
+        numFrame += 1
+        cv2.imwrite(outDir + oriname, im)
+        smap = sal.compute_saliency(outDir + oriname)  # open salient need file path
+        cv2.imwrite(outDir + salname, smap*255)
+        np.savetxt(outDir + txtname, smap*255, fmt="%d")
+        print oriname, salname, txtname, "saved in ", outDir
+        success, im = video.read()
     
 
